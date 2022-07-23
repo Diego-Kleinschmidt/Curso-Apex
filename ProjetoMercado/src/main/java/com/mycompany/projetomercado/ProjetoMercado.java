cef3c532c74474b40958d5215e5b9edc3eb2122d
Diego Henrique Kleinschmidt <dieklei@hotmail.com>
2022-07-22 22:58:19 -0300

    22/07/22

--- ProjetoMercado/src/main/java/com/mycompany/projetomercado/ProjetoMercado.java
+++ ProjetoMercado/src/main/java/com/mycompany/projetomercado/ProjetoMercado.java
@@ -1,19 +1,11 @@
 package com.mycompany.projetomercado;
 
-import DAO.ComandosDAO;
-import DTO.ClientesDTO;
 import javax.swing.JOptionPane;
 
 public class ProjetoMercado {
 
     public static void main(String[] args) {
-        ClientesDTO cli = new ClientesDTO();
-        ComandosDAO coamndo = new ComandosDAO();
-        cli.setNome_cliente("Diego Henrique Kleinschmidt");
-        cli.setIdade(26);
-        cli.setSituacao('A');
-        // String comando = coamndo.insereRegistro(cli);
-       // JOptionPane.showMessageDialog(null, comando);
+        JOptionPane.showMessageDialog(null, "Teste");
 
     }
 }
